9df8dec07ac2ab6d390e4f31920ab64e6a09b054
akafael <raffaellimma@gmail.com>
2013-07-03 23:55:49 -0300

    Added print to .dat function

--- num/list.lua
+++ num/list.lua
@@ -9,10 +9,11 @@ Nodea = { __index = Node }
 -- Function to create a new instance of Node
 function Node:new(value,left, right)
    local new_inst = {}    -- the new instance
-   local v = value
    setmetatable( new_inst, Nodea ) -- all instances share the same metatable
 
    self.value = v
+   self.t = 0
+   self.x = 0
    self.left = left or self
    self.right = right or self
    
@@ -21,6 +22,11 @@ end
 
 function Node:set_value(value)
    self.value = value
+end
+
+function Node:set_pos(left,right)
+   self.left = left or self.left
+   self.right = right or self.right
 end
 
 function test()
--- num/numKDV.lua
+++ num/numKDV.lua
@@ -9,6 +9,8 @@
 --   f_0 = e^{-x^2}
 --]]
 
+require "num/list" -- package to do the nodes of vector
+
 -- default table values
 H = 0.01 -- value to 'h'
 C = 1 -- value to c constant
@@ -23,14 +25,15 @@ end
 function d1a2(u,h)
   local h = h or H
   local u = u
-  return((u.left.v-u.right.v)/(2*h))
+  return((u.left.value-u.right.value)/(2*h))
 end
 
 -- Third Derivate whith accuracy 2
 function d3a2(u,h)
   local h = h or H
   local u = u
-  return((-(0.5)*(u.left.left.v) + u.left.v - u.right.v +(0.5)*(u.right.right.v))/(h^3))
+  return((-(0.5)*(u.left.left.value) + u.left.value -
+    u.right.value +(0.5)*(u.right.right.value))/(h^3))
 end
 
 -- Macro to derivate in order n
@@ -62,9 +65,9 @@ function kdvStep(u,c,k,h,dt)
 end
 
 function kdvStepVector(u,c,k,h,dt)
-  local u0 = u0
-  for u0 in u do
-    kdvStep(u0,c,k,h,dt)
+  local u = u
+  for i,u1 in ipairs(u) do
+     u[i] = kdvStep(u1,c,k,h,dt)
   end
   return(u0)
 end
@@ -75,20 +78,46 @@ function u_f0(min_x,max_x,dx)
   local min_x = min_x or -3
   local max_x = max_x or 3
 
-  local u0 = {u0} u0.left = u0[1] u0.right = u0[1]
+  local u0 = {}
   for i=-3, 3,0.1 do
-    local u = {} u.left = u0[#u0] u.right = u0[#u0]
+    local u = Node:new()
+    u:set_pos(u0[#u0],u0[1])
     u.left.right = u
     u.right.left = u
-    u.v = f0(i)
-    -- u.x = i
-    -- u.t = 0
+
+    u.value = f0(i)
+    u.x = i
+    u.t = 0
+
     u0[#u0+1] = u
   end
 
   return(u0)
 end
 
+-- Calculate the values to kdv wave before 't' time:
+function kdv_t(u,t)
+   local u0 = u or u_f0()
+   for i=0,t,H do
+      u0 = kdvStepVector(u,c,k,h,H)
+   end
+   return u0
+end
+
+function printDat(u,filename)
+   local file = io.open(filename,"w")
+
+   local data = {}
+   for _, u0 in ipairs(u) do
+      data[#data+1] = table.concat({tostring(u0.t),
+				   tostring(u0.x),
+				   tostring(u0.value)},
+				   "\t")
+   end
+
+   file:write(table.concat(data,"\n"))
+   file:close()
+end
+
 -- TODO
--- function to Print a .dat
--- Add 't' and 'x' values to the vector 'u'
+-- > Function to find any value of u(x,t)
