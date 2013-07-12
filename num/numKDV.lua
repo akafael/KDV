cc50bcd4a84355e2f66d732c42f7ac7f546febeb
Rafael Lima <rafael@taylor.(none)>
2013-07-12 13:59:11 -0300

    Aplied Upwind scheme : change the first derivative in x to backward derivative

--- num/numKDV.lua
+++ num/numKDV.lua
@@ -12,66 +12,97 @@
 require "list" -- package to do the nodes of vector
 
 -- default table values
-H = 0.01 -- value to 'h'
+H = 0.1 -- value to 'h'
+DT = 0.01 -- value to 'dt'
 C = 1 -- value to c constant
 K = 1 -- value to k constant
 
+ZOOM_X = 1
+ZOOM_Y = 1
+
 -- Define initial condition
 function f0(x)
- return(math.exp(-(x^2)))
+ return(ZOOM_Y*math.exp(-(x^2)))
 end
 
--- First Derivate whith accuracy 2
-function d1a2(u,h)
+-- First Central Derivative with accuracy 2
+function d1c2(u,h)
   local h = h or H
   local u = u
   return((u.right.value-u.left.value)/(2*h))
 end
 
--- Third Derivate whith accuracy 2
-function d3a2(u,h)
-  local h = h or H
+-- First backward Derivative with accuracy 2 
+function d1b2(u,h)
+   -- −3/2 2 −1/2
+   local h = h or H
+   local u = u
+   return(((-3)*u.value+
+	   (4)*u.left.value+
+	   (-1)*u.left.value)/(2*h))
+end
+
+-- Third Central Derivative with accuracy 2
+function d3c2(u,h)
+   local h = h or H
   local u = u
   return((-(0.5)*(u.left.left.value) + u.left.value -
     u.right.value +(0.5)*(u.right.right.value))/(h^3))
 end
 
--- Macro to derivate in order n
+-- Macro to derivative in order n
 function diff(u,n,h)
   local n = n or 0
 
   if n==0 then
     return(u)
   elseif n==1 then
-    return(d1a2(u,h))
+    return(d1b2(u,h))
   elseif n==3 then
-    return(d3a2(u,h))
+    return(d3c2(u,h))
   else
     return(nil)
   end
 end
 
--- Calculate U(x,t+dt) based in foward derivate
+-- Calculate U(x,t+dt) based in forward derivate
 function kdvStep(u,c,k,h,dt)
   -- set default values
   local c = c or C
   local k = k or K
-  local dt = dt or H
+  local dt = dt or DT
 
   -- calc U(x,t+dt)
-  local u1 = u
-  u1.value = -c*dt*diff(u,1,h) - k*dt*diff(u,3,h)
-  u1.t = u1.t+dt
+  local u1 = Node:new()
+ 
+  -- TODO Check this line:
+  -- u1.value = -c*dt*diff(u,1,h) + k*dt*diff(u,3,h)
+  u1.value = -c*dt*diff(u,1,h) + k*dt*diff(u,3,h) + u.value
+
+  u1.t = u.t+dt
+  u1.x = u.x
 
   return(u1)
 end
 
 function kdvStepVector(u,c,k,h,dt)
-  local u0 = u
-  for i,u1 in ipairs(u) do
-     u0[i] = kdvStep(u1,c,k,h,dt)
+  local listu1 = u or u_f0()
+  local listu = {}
+  for _,u1 in ipairs(listu1) do
+     local n = Node:new()
+     n:set_pos(listu[#listu],listu[1])
+     n.left.right = n
+     n.right.left = n
+
+     u0 = kdvStep(u1,c,k,h,dt)
+
+     n.value = u0.value
+     n.x = u0.x
+     n.t = u0.t
+
+     listu[#listu + 1] = n
   end
-  return(u0)
+  return(listu)
 end
 
 -- Initial condition:
@@ -83,7 +114,7 @@ function u_f0(min_x,max_x,dx)
   local u0 = {}
   for i=min_x, max_x, dx do
     local u = Node:new()
-    u:set_pos(u0[i],u0[min_x])
+    u:set_pos(u0[#u0],u0[1])
     u.left.right = u
     u.right.left = u
 
@@ -91,7 +122,7 @@ function u_f0(min_x,max_x,dx)
     u.x = i
     u.t = 0
 
-    u0[i+dx] = u
+    u0[#u0+1] = u
   end
 
   return(u0)
@@ -100,15 +131,34 @@ end
 -- Calculate the values to kdv wave before 't' time:
 function kdv_t(u,t)
    local u0 = u or u_f0()
-   for i=0,t,H do
-      u0 = kdvStepVector(u,c,k,h,H)
+   for h=0,t,H do
+      u0 = kdvStepVector(u0,c,k,H,DT)
    end
    return u0
 end
 
--- Print file whith u(x,t) in a fixed t
-function printDat(u,filename)
-   local file = io.open(filename,"w")
+function kdv_x(u,x)
+   -- BUG returning nil..
+   local u0  = {}
+   for i,u0 in ipairs(u) do
+      if (math.abs(u0.x - x)<0.1) then
+	 break
+      end
+   end
+
+   return u0
+end
+
+function kdv(x,t)
+   local u = u_f0()
+   u = kdv_t(u,t)
+   u = kdv_x(u,x)
+   return u
+end
+
+-- Print file with u(x,t) in a fixed t
+function printDat(filename,u)
+   local file = io.open("data/"..filename,"w")
 
    local data = {}
    for _, u0 in ipairs(u) do
@@ -122,6 +172,26 @@ function printDat(u,filename)
    file:close()
 end
 
+-- Print files whith  u(x,ti) with ti = [0,t]
+function printKDV_t(listu,t)
+
+   local u0 = u_f0()
+   print(table.concat({"using DT =",DT,"H=",H,",C=",C}," "))
+
+   if(t>0) then
+      for h=0,t,DT do
+	 u0 = kdvStepVector(u0)
+	 print("drawing t = "..u0[1].t)
+	 local filename = {"ut",tostring(h/DT),".dat"}
+	 printDat(table.concat(filename),u0)
+      end
+   else
+      printDat("ut0.dat")
+   end
+
+   print("[gnuplot:]")
+   print(io.popen("gnuplot < graphConfig.plot")..'\n')
+end
+
 -- TODO
 -- > Function to find any value of u(x,t)
--- > A matrix to map (x,t) value
